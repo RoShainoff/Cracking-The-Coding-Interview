e800d679d3c49830f5b9769b3847f2bf08007216
Шайнов Роман Владиславович <shainovrv@ad.atlant.by>
2022-05-11 18:26:24 +0300

    Add project files.

---
+++ ConsoleApp1/Code/Chapter 1/Question1_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Code.Chapter_1
+{
+    public static class Question1_1
+    {
+
+
+        // Space: O(N)
+        // Time: O(N)
+        public static bool AreAllCharactersUnique(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            HashSet<char> hashSet = new HashSet<char>();
+
+            foreach (var symbol in input)
+            {
+                if (!hashSet.Add(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Space: O(1)
+        // Time: O(N^2)
+        public static bool AreAllCharactersUniqueNoAdditionalMemory(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
