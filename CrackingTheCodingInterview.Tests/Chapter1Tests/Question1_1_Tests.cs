d4436d373da7b831f575c97fb63c74500a2a0b1b
shainovrv <roman.shainov@yahoo.com>
2022-05-11 20:12:43 +0300

    Tests was added

---
+++ CrackingTheCodingInterview.Tests/Chapter1Tests/Question1_1_Tests.cs
@@ -0,0 +1,48 @@
+﻿using CrackingTheCodingInterview.Code.Chapter_1;
+using Xunit;
+
+namespace CrackingTheCodingInterview.Tests.Chapter1Tests
+{
+    public class Question1_1_Tests
+    {
+        [Fact]
+        public void AreAllCharactersUnique_CorrectTest()
+        {
+            // Act
+            var result = Question1_1.AreAllCharactersUnique("123456");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreAllCharactersUnique_IncorrectTest()
+        {
+            // Act
+            var result = Question1_1.AreAllCharactersUnique("123456_123456");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AreAllCharactersUniqueNoAdditionalMemory_CorrectTest()
+        {
+            // Act
+            var result = Question1_1.AreAllCharactersUniqueNoAdditionalMemory("123456");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreAllCharactersUniqueNoAdditionalMemory_IncorrectTest()
+        {
+            // Act
+            var result = Question1_1.AreAllCharactersUniqueNoAdditionalMemory("123456_123456");
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview.Tests/Chapter1Tests/Question1_2_Tests.cs
@@ -0,0 +1,68 @@
+﻿using CrackingTheCodingInterview.Code.Chapter_1;
+using Xunit;
+
+namespace CrackingTheCodingInterview.Tests.Chapter1Tests
+{
+    public class Question1_2_Tests
+    {
+        [Fact]
+        public void AreStringsPermutation_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutation("123456", "123456");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreStringsPermutation_String1IsEmpty_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutation("", "123456");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AreStringsPermutation_String2IsEmpty_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutation("123456", "");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AreStringsPermutationNoSort_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutationNoSort("123456", "123456");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreStringsPermutationNoSort_String1IsEmpty_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutationNoSort("", "123456");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AreStringsPermutationNoSort_String2IsEmpty_CorrectTest()
+        {
+            // Act
+            var result = Question1_2.AreStringsPermutationNoSort("123456", "");
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
